b97fba76cc5133d1d8c4444cc73e239966288d6c
luowailin <luowailin@luowailindeMac-mini.local>
2019-02-28 16:15:24 +0800

    纹理

--- OpenGLTest_2/OpenGLTest_2/main.cpp
+++ OpenGLTest_2/OpenGLTest_2/main.cpp
@@ -11,15 +11,26 @@
 #include <cmath>
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
+#include "Shader.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 using namespace std;
 void framebuffer_size_callback(GLFWwindow *window, int width, int height);
 void processInput(GLFWwindow *window);
 
+//更改的颜色三角形
+void renderChangeTriangle(GLFWwindow *window);
+//不同颜色的三角形
+void renderDifferentColorTriangle(GLFWwindow *window);
+void renderRectAndTexture(GLFWwindow *window);
+void renderRectAndTwoTexture(GLFWwindow *window);
+
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSouce = "#version 330 core\n"
+const char *vertexShaderSource = "#version 330 core\n"
 "layout (location = 0) in vec3 aPos;\n"
 "void main()\n"
 "{\n"
@@ -34,7 +45,23 @@ const char *fragmentShaderSource = "#version 330 core\n"
 " FragColor = ourColor;\n"
 "}\n\0";
 
+const char *vertexShaderSource2 = "#version 330 core\n"
+"layout (location = 0) in vec3 aPos;\n"
+"layout (location = 1) in vec3 aColor;\n"
+"out vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+"gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+"ourColor = aColor;\n"
+"}\n\0";
 
+const char *fragmentShaderSource2 = "#version 330 core\n"
+"out vec4 FragColor;\n"
+"in vec3 ourColor;\n"
+"void main()\n"
+"{\n"
+" FragColor = vec4(ourColor, 1.0f);\n"
+"}\n\0";
 int main(int argc, char **argv){
     
     glfwInit();
@@ -64,10 +91,18 @@ int main(int argc, char **argv){
     glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     
+    //renderChangeTriangle(window);
+    //renderDifferentColorTriangle(window);
+    renderRectAndTexture(window);
     
+    glfwTerminate();
+    return 0;
+}
+
+void renderChangeTriangle(GLFWwindow *window){
     unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
     unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSouce, NULL);
+    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
     glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
     glCompileShader(vertexShader);
     glCompileShader(fragmentShader);
@@ -79,7 +114,7 @@ int main(int argc, char **argv){
     
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
-
+    
     float vertices[] = {
         -0.5f, -0.5f, 0.0f,   //left
         0.5f, -0.5f, 0.0f,   //right
@@ -106,13 +141,14 @@ int main(int argc, char **argv){
         glClear(GL_COLOR_BUFFER_BIT);
         
         
+        //uniform GLSL shader中的全局常量，可以在任意shader访问，不同的shader中uniform是一起链接的，初始化之后不能修改值
         float timeValue = glfwGetTime();
         float greenValue = (sin(timeValue) / 2.0f) + 0.5f;
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor"); //获取uniform的位置
         
         glUseProgram(shaderProgram);
         
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f); //传给Shader
         
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -122,9 +158,155 @@ int main(int argc, char **argv){
         glfwPollEvents();
     }
     
-    glfwTerminate();
-    return 0;
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 }
+
+void renderDifferentColorTriangle(GLFWwindow *window){
+    
+    Shader ourShader("shader.vs", "shader.fs");
+    
+    float vertices[] = {
+        // 位置              // 颜色
+        0.5f, -0.5f, 0.0f,  1.0f, 0.0f, 0.0f,   // 右下
+        -0.5f, -0.5f, 0.0f,  0.0f, 1.0f, 0.0f,   // 左下
+        0.0f,  0.5f, 0.0f,  0.0f, 0.0f, 1.0f    // 顶部
+    };
+    
+    unsigned int VAO, VBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    //顶点数据
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 6*sizeof(GL_FLOAT), (void *)0);
+    glEnableVertexAttribArray(0);
+    //颜色数据
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 6*sizeof(GL_FLOAT), (void *)(3* sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(1); //这个1 代表的shader里的location
+    
+    glBindVertexArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+
+
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        
+        ourShader.use();
+        glBindVertexArray(VAO);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glBindVertexArray(0);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+}
+
+//
+void renderRectAndTexture(GLFWwindow *window){
+    Shader ourShader("4.1.texture.vs", "4.1.texture.fs");
+
+    float vertices[] = {
+        // positions          // colors           // texture coords
+        0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f, // top right
+        0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f, // bottom right
+        -0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f, // bottom left
+        -0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f  // top left
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_TRUE, 8 * sizeof(GL_FLOAT), (void *)0);
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_TRUE, 8 * sizeof(GL_FLOAT), (void *)(3 * sizeof(GL_FLOAT)));
+    glVertexAttribPointer(2, 2, GL_FLOAT, GL_TRUE, 8 * sizeof(GL_FLOAT), (void *)(6 * sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
+    glEnableVertexAttribArray(2);
+    
+//    glBindVertexArray(0);
+//    glBindBuffer(GL_ARRAY_BUFFER, 0);
+//    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
+    
+    //纹理
+    unsigned int texture;
+    glGenTextures(1, &texture);
+    glBindTexture(GL_TEXTURE_2D, texture);
+    
+    //环绕方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    
+    //过淲方式
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    
+    //多级渐远处理
+    /*glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR); //注意多级渐远处理只对纹理缩小才有作用，放大不会使用渐远处理可能会报错
+    */
+    
+    
+    int width, height, nrChannels;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannels, 0);
+    /** glTexImage2D()
+     第二个参数:为纹理指定多级渐远纹理的级别，如果你希望单独手动设置每个多级渐远纹理级别的话，就填0代理基本级别
+     第三个参数:希望把纹理储存为何种格式
+     第六个参数：永远设置为0
+     第七、八个参数：源图的格式和数据类型
+     */
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D); //要使用多级渐远纹理，我们必须手动设置所有不同的图像 或者 直接调用glGenerateMipmap这会为当前绑定的纹理自动生成所需要的多级渐远处理
+        
+        stbi_image_free(data); //释放数据
+    }
+    
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        ourShader.use();
+        glBindTexture(GL_TEXTURE_2D, texture);
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
+
+void renderRectAndTwoTexture(GLFWwindow *window){
+    
+}
+
 
 //窗口被调整的时候回调函数
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
