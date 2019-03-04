a1b2059617e201b209d11fa4a4231d12d7ce200d
luowailin <luowailin@luowailindeMac-mini.local>
2019-03-04 14:44:50 +0800

    旋转和缩放，翻转

--- OpenGLTest_2/OpenGLTest_2/main.cpp
+++ OpenGLTest_2/OpenGLTest_2/main.cpp
@@ -13,6 +13,10 @@
 #include <GLFW/glfw3.h>
 #include "Shader.hpp"
 
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
 
@@ -27,6 +31,8 @@ void renderDifferentColorTriangle(GLFWwindow *window);
 void renderRectAndTexture(GLFWwindow *window);
 void renderRectAndTwoTexture(GLFWwindow *window);
 void renderChangeVisibilityTexture(GLFWwindow *window);
+//旋转
+void renderTransformTexture(GLFWwindow *window);
 
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
@@ -99,7 +105,8 @@ int main(int argc, char **argv){
     //renderDifferentColorTriangle(window);
     //renderRectAndTexture(window);
     //renderRectAndTwoTexture(window);
-    renderChangeVisibilityTexture(window);
+    //renderChangeVisibilityTexture(window);
+    renderTransformTexture(window);
     
     glfwTerminate();
     return 0;
@@ -511,7 +518,121 @@ void renderChangeVisibilityTexture(GLFWwindow *window){
     glDeleteBuffers(1, &EBO);
 }
 
-
+void renderTransformTexture(GLFWwindow *window){
+    Shader ourShader("4.6.texture.vs", "4.6.texture.fs");
+    
+    float vertices[] = {
+        0.5f,   0.5f,  0.0f,    1.0f, 1.0f,
+        0.5f,  -0.5f,  0.0f,    1.0f, 0.0f,
+        -0.5f, -0.5f,  0.0f,    0.0f, 0.0f,
+        -0.5f,  0.5f,  0.0f,    0.0f, 1.0f
+    };
+    unsigned int indices[] = {
+        0, 1, 3, // first triangle
+        1, 2, 3  // second triangle
+    };
+    unsigned int VAO, VBO, EBO;
+    glGenVertexArrays(1, &VAO);
+    glGenBuffers(1, &VBO);
+    glGenBuffers(1, &EBO);
+    
+    glBindVertexArray(VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void *)0);
+    glEnableVertexAttribArray(0);
+    
+    glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(GL_FLOAT), (void *)(3 * sizeof(GL_FLOAT)));
+    glEnableVertexAttribArray(1);
+    
+    unsigned int texture0, texture1;
+    glGenTextures(1, &texture0);
+    glBindTexture(GL_TEXTURE_2D, texture0);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    
+    stbi_set_flip_vertically_on_load(true);
+    int width, height, nrChannel;
+    unsigned char *data = stbi_load("container.jpg", &width, &height, &nrChannel, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+    
+    glGenTextures(1, &texture1);
+    glBindTexture(GL_TEXTURE_2D, texture1);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    data = stbi_load("awesomeface.png", &width, &height, &nrChannel, 0);
+    if (data) {
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    stbi_image_free(data);
+    
+    /*
+    glm::mat4 trans;
+    trans = glm::rotate(trans, glm::radians(90.0f), glm::vec3(0.0,0.0,1.0));
+    trans = glm::scale(trans, glm::vec3(0.5, 0.5, 0.5));  //每个轴都缩放到0.5倍
+    */
+    
+    ourShader.use();
+    ourShader.setInt("texture1", 0);
+    ourShader.setInt("texture2", 1);
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glActiveTexture(GL_TEXTURE0);
+        glBindTexture(GL_TEXTURE_2D, texture0);
+        glActiveTexture(GL_TEXTURE1);
+        glBindTexture(GL_TEXTURE_2D, texture1);
+        
+        glm::mat4 transform = glm::mat4(1.0f);
+        //transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f));
+        transform = glm::translate(transform, glm::vec3(0.5f, -0.5f, 0.0f));
+        transform = glm::rotate(transform, (float)glfwGetTime(), glm::vec3(0.0f, 0.0f, 1.0f)); //第一个参数 用来保存的值  第二个参数:旋转的角度(glm是弧度制) 第三个参数是指围绕哪个面旋转(0.0, 0.0, 1.0) 表示围绕Z轴
+        /*
+         如果先平移后旋转和先旋转后平移的结果是不一样的，为什么?
+         */
+        
+        
+        
+       //ourShader.use();
+        unsigned int transformLoc = glGetUniformLocation(ourShader.ID, "transform");
+        //把矩阵数据发送给着色器
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, glm::value_ptr(transform));//第一个参数，uniform的位置值， 第二个参数将要发送多少个矩阵 第三个参数 是否希望对我们的矩阵进行置换，也就是说交换我们的矩阵的行和列   第四个参数真正的矩阵数据
+        
+        glBindVertexArray(VAO);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        
+        //second transformation
+        transform = glm::mat4(1.0f);
+        transform = glm::translate(transform, glm::vec3(-0.5f, 0.5f, 1.0f));
+        float scaleAmount = sin(glfwGetTime());  //sin函数在这里会很有用，不过注意使用sin函数时应用负值会导致物体被翻转
+        transform = glm::scale(transform, glm::vec3(scaleAmount, scaleAmount, scaleAmount));
+        glUniformMatrix4fv(transformLoc, 1, GL_FALSE, &transform[0][0]);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
+    glDeleteBuffers(1, &EBO);
+}
 
 //窗口被调整的时候回调函数
 void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
