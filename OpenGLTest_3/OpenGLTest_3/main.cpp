9b190e35d71024d686b97d4c4eff1c269642fe19
luowailin <luowailin@luowailindeMac-mini.local>
2019-03-06 15:20:23 +0800

    坐标

--- OpenGLTest_3/OpenGLTest_3/main.cpp
+++ OpenGLTest_3/OpenGLTest_3/main.cpp
@@ -452,7 +452,8 @@ void renderTextureMore3D(GLFWwindow *window){
             glm::mat4 model = glm::mat4(1.0f);
             model = glm::translate(model, cubePositions[i]);
             
-            float angle = 20.0 * i;
+            float angle = 20.0 * glfwGetTime();
+            cout << angle << endl;
             model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
             ourShader.setMat4("model", model);
             glDrawArrays(GL_TRIANGLES, 0 , 36);
