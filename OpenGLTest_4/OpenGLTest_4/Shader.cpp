c3fd34bc35a21bce09bb8ae9e654789c5c2a2ee4
luowailin <luowailin@luowailindeMac-mini.local>
2019-03-07 18:11:16 +0800

    初始化项目

---
+++ OpenGLTest_4/OpenGLTest_4/Shader.cpp
@@ -0,0 +1,102 @@
+//
+//  Shader.cpp
+//  OpenGLTest_2
+//
+//  Created by luowailin on 2019/2/28.
+//  Copyright © 2019 luowailin. All rights reserved.
+//
+
+#include "Shader.hpp"
+
+using namespace std;
+
+Shader::Shader(const GLchar *vertexPath, const GLchar *fragmentPath){
+    string vertexCode;
+    string fragmentCode;
+    ifstream vShaderFile;
+    ifstream fShaderFile;
+    
+    //抛出异常
+    vShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
+    fShaderFile.exceptions(ifstream::failbit|ifstream::badbit);
+    
+    try {
+        vShaderFile.open(vertexPath);
+        fShaderFile.open(fragmentPath);
+        
+        stringstream vShaderStream, fShaderStream;
+        
+        vShaderStream << vShaderFile.rdbuf();
+        fShaderStream << fShaderFile.rdbuf();
+        
+        vShaderFile.close();
+        fShaderFile.close();
+        
+        vertexCode = vShaderStream.str();
+        fragmentCode = fShaderStream.str();
+        
+    } catch (ifstream::failure e) {
+        cout<<"ERROR::SHADER::FILE_NOT_SUCCESFULLY_READ"<<endl;
+    }
+    
+    const char *vShaderCode = vertexCode.c_str();
+    const char *fShaderCode = fragmentCode.c_str();
+    
+    unsigned int vertex, fragment;
+    vertex = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vertex, 1, &vShaderCode, NULL);
+    glCompileShader(vertex);
+    checkCompileErrors(vertex, "VERTEX");
+    
+    fragment = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fragment, 1, &fShaderCode, NULL);
+    glCompileShader(fragment);
+    checkCompileErrors(fragment, "FRAGMENT");
+    
+    ID = glCreateProgram();
+    glAttachShader(ID, vertex);
+    glAttachShader(ID, fragment);
+    glLinkProgram(ID);
+    checkCompileErrors(ID, "PROGRAM");
+    
+    glDeleteShader(vertex);
+    glDeleteShader(fragment);
+}
+
+void Shader::use(){
+    glUseProgram(ID);
+}
+
+void Shader::setBool(const std::string &name, bool value) const{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), (int)value);
+}
+
+void Shader::setInt(const std::string &name, int value) const{
+    glUniform1i(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setFloat(const std::string &name, float value) const{
+    glUniform1f(glGetUniformLocation(ID, name.c_str()), value);
+}
+
+void Shader::setMat4(const std::string &name, const glm::mat4 &mat) const{
+    glUniformMatrix4fv(glGetUniformLocation(ID, name.c_str()), 1, GL_FALSE, &mat[0][0]);
+}
+
+void Shader::checkCompileErrors(GLuint shader, string type){
+    GLint success;
+    GLchar infoLog[1024];
+    if (type != "PROGRAM") {
+        glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+        if (!success) {
+            glGetShaderInfoLog(shader, 1024, NULL, infoLog);
+            cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << ":"<< infoLog <<endl;
+        }
+    } else {
+        glGetProgramiv(shader, GL_LINK_STATUS, &success);
+        if (!success) {
+            glGetProgramInfoLog(shader, 1024, NULL, infoLog);
+             cout << "ERROR::PROGRAM_LINK_ERROR of type: " << type << ":"<< infoLog <<endl;
+        }
+    }
+}
---
+++ OpenGLTest_4/OpenGLTest_4/main.cpp
@@ -0,0 +1,92 @@
+//
+//  main.cpp
+//  OpenGLTest_4
+//
+//  Created by luowailin on 2019/3/7.
+//  Copyright © 2019 luowailin. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include "Shader.hpp"
+#include "Camera.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
+
+using namespace std;
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+void processInput(GLFWwindow *window);
+void mouse_callback(GLFWwindow *window, double xpos, double ypos);
+void scroll_callback(GLFWwindow *window, double xOffset, double yOffset);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+int main(int argc, char **argv){
+    
+    glfwInit();
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
+#if __APPLE__
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+    
+    GLFWwindow *window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "摄像机", NULL, NULL);
+    if (window == NULL) {
+        cout << "Failed to create GLFW window" << endl;
+        glfwTerminate();
+        return -1;
+    }
+    
+    glfwMakeContextCurrent(window);
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        cout << "Faile to initialize GLAD" << endl;
+        return -1;
+    }
+    
+    glViewport(0, 0, SCR_WIDTH, SCR_HEIGHT);
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    glfwSetCursorPosCallback(window, mouse_callback);
+    glfwSetScrollCallback(window, scroll_callback);
+    
+    while (!glfwWindowShouldClose(window)) {
+        processInput(window);
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+        
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height){
+    glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window){
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        glfwSetWindowShouldClose(window, true);
+    }
+}
+
+void mouse_callback(GLFWwindow *window, double xpos, double ypos){
+    
+}
+
+void scroll_callback(GLFWwindow *window, double xOffset, double yOffset){
+    
+}
