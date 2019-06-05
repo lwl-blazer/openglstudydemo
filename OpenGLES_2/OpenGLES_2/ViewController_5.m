38776e3374e9d22fca8c2f4f1d0f1c11515ba6f4
luowailin <blazer402@126.com>
2019-06-05 17:51:27 +0800

    add

--- OpenGLES_2/OpenGLES_2/ViewController_5.m
+++ OpenGLES_2/OpenGLES_2/ViewController_5.m
@@ -10,7 +10,7 @@
 #import "AGLKContext.h"
 #import "AGLKVertexAttribArrayBuffer.h"
 #import "GLKEffectPropertyTexture+AGLKAdditions.h"
-
+#import "AGLKShader.h"
 
 enum
 {
@@ -88,6 +88,7 @@ static const SceneVertex vertices[] =
 
 @property(nonatomic, strong) GLKBaseEffect *baseEffect;
 @property(nonatomic, strong) AGLKVertexAttribArrayBuffer *vertexBuffer;
+@property(nonatomic, strong) AGLKShader *shader;
 
 @end
 
@@ -100,8 +101,24 @@ static const SceneVertex vertices[] =
     NSAssert([view isKindOfClass:[GLKView class]], @"is not a GLKView");
     
     view.context = [[AGLKContext alloc] initWithAPI:kEAGLRenderingAPIOpenGLES3];
+    /** GLK配置缓冲区
+     * drawableDepthFormat   深度缓冲区
+     *  深度缓冲区保证离观察者更近的物体"盖住"远的物体
+     *
+     * drawableColorFormat
+     * 保存颜色值的缓冲区 默认的颜色格式 GLKViewDrawableColorFormatRGBA8888  代表每个颜色成分占用8个位，所以每个像素占用4个字节
+     *
+     * drawableStencilFormat  蒙版缓冲区
+     * 蒙版缓冲区是限制我们只能在屏幕特定部分绘制图形，可以用于影子的绘制，如果墙只能容纳你影子的一半，另外一半就需要用蒙版技术去掉
+     * 默认值是GLKViewDrawableStencilFormatNone 可以设置属性为GLKViewDrawableStencilFormat8 打开模板缓冲区
+     *
+     * drawableMultisample   多次采样缓冲区
+     * 如果使用OpenGL 划线，发现线上有锯齿，多次采样可以解决这个问题，基本的做法是：不再是对于每个像素调用一次frag渲染器，而是把一个像素分成更小的单位多次调用frag渲染器。然后融合多次渲染得到的颜色值，这样最终的结果就会更加平滑。 这个属性将消耗更多的资源   默认值是GLKViewDrawableMultisampleNone  可以设置 GLKViewDrawableMultisample4X打开这个选项
+     *
+     * 其它的drawableHeight/drawableWidth  snapshot  bindDrawable  deleteDrawable
+     */
     view.drawableDepthFormat = GLKViewDrawableDepthFormat24;
-    
+   
     [AGLKContext setCurrentContext:view.context];
     
     [self loadShaders];
@@ -149,22 +166,29 @@ static const SceneVertex vertices[] =
                                            value:GL_REPEAT];
 }
 
-
+//公转自转
 - (void)update{
+    //取绝对值
     float aspect = fabs(self.view.bounds.size.width / self.view.bounds.size.height);
     
-    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f), aspect, 0.1f, 100.0f);
+    //创建projectionMatrix 透视投影矩阵
+    GLKMatrix4 projectionMatrix = GLKMatrix4MakePerspective(GLKMathDegreesToRadians(65.0f),  //角度转弧度
+                                                            aspect,
+                                                            0.1f,
+                                                            100.0f);   //参数3和4 表示可视深度 必须是正数
     self.baseEffect.transform.projectionMatrix = projectionMatrix;
     
-    GLKMatrix4 baseModelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -4.0f);
+    //模型视图矩阵
+    //第一个正方形(大的)
+    GLKMatrix4 baseModelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -4.0f);   //把正方形从原点往z方向移-4
     baseModelViewMatrix = GLKMatrix4Rotate(baseModelViewMatrix, rotation, 0.0f, 1.0f, 0.0f);
-    
     GLKMatrix4 modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, -1.5f);
     modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, rotation, 1.0f, 1.0f, 1.0f);
     modelViewMatrix = GLKMatrix4Multiply(baseModelViewMatrix, modelViewMatrix);
     
     self.baseEffect.transform.modelviewMatrix = modelViewMatrix;
     
+    //第二个正方形(小的)
     modelViewMatrix = GLKMatrix4MakeTranslation(0.0f, 0.0f, 1.5f);
     modelViewMatrix = GLKMatrix4Rotate(modelViewMatrix, rotation, 1.0f, 1.0f, 1.0f);
     modelViewMatrix = GLKMatrix4Multiply(baseModelViewMatrix, modelViewMatrix);
@@ -173,6 +197,16 @@ static const SceneVertex vertices[] =
     modelViewProjectionMatrix = GLKMatrix4Multiply(projectionMatrix, modelViewMatrix);
     
     rotation += self.timeSinceLastUpdate * 0.5f;
+    
+    /**
+     * OpenGL ES中有两套矩阵  都是4 * 4
+     *
+     * 1.modelview matrix 是用来对虚拟世界进行变换的矩阵。要对虚拟世界中的物体进行旋转，转移或尺寸变化，都是修改此矩阵
+     *
+     * 2.project matrix  用来创建根据的设定的视口对世界坐标进行描述的二维表示
+     *
+     * 一个3*3的矩阵可以描述绕做生意轴旋转任何角度的情况。然而为表示可能遇到的任何变换，我们仍然需要第四行/列。第四列用来保存变换信息，第四行用来表示透视变换
+     */
 }
 
 
@@ -203,9 +237,9 @@ static const SceneVertex vertices[] =
     [self.vertexBuffer drawArrayWithMode:GL_TRIANGLES
                         startVertexIndex:0 numberOfVertices:sizeof(vertices)/sizeof(SceneVertex)];
     
+    
     //给shader 设置值
     glUseProgram(program);
-    
     glUniformMatrix4fv(uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX], 1, 0, modelViewProjectionMatrix.m);
     glUniformMatrix3fv(uniforms[UNIFORM_NORMAL_MATRIX], 1, 0, normalMatrix.m);
     glUniform1i(uniforms[UNIFORM_TEXTURE0_SAMPLER2D], 0);
@@ -240,7 +274,6 @@ static const SceneVertex vertices[] =
     glBindAttribLocation(program, GLKVertexAttribTexCoord0, "aTextureCoord0");
     glBindAttribLocation(program, GLKVertexAttribTexCoord1, "aTextureCoord1");
     
-
     if (![self linkProgram:program]) {
         NSLog(@"faile to link program");
         
